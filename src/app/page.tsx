50f1eb245b8473d845a2efc313d792249c22b058
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-01-23 23:10:22 +0600

    added logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       This is Home page
       <br />
       <Button>Shadcn Button</Button>
       <button className="btn btn-primary">DaisyUI Button</button>
-    </div>
+    </>
   );
 }
