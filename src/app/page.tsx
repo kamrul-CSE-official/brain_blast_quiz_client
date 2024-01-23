41664ca338a5b1139fccc936f551512cb045548c
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-01-23 22:14:02 +0600

    setup shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
     <div>
       This is Home page
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+      <br />
+      <Button>Shadcn Button</Button>
+      <button className="btn btn-primary">DaisyUI Button</button>
     </div>
   );
 }
